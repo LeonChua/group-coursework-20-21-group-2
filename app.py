c7950ae02668d1d3937b64d85ee98a0077417a92
mazab <mazabdul7@outlook.com>
2020-11-19 14:30:33 +0000

    created basic Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from Flask import Flask, render_template
+
+app = Flask(__name__) # creates Flask object from main fcn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__': #runs Flask app
+    app.run()
