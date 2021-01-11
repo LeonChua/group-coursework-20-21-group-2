1e1a76ce570c493128ba9a8a727670a3dc0d8f3d
mazab <mazabdul7@outlook.com>
2021-01-11 20:07:29 +0000

    Added Mazin Classes and test

--- models.py
+++ models.py
@@ -78,3 +78,46 @@ class User(object):
     def __str__(self):
         info = "Name: " + self._name, " Email: " + self._email, " University: " + self._university, " ID: " + self._userId
         return info
+
+
+#Mazin Abdulmahmood - Search Class
+class search(object):
+
+    def getResult(self, data, index):
+        #Returns the data from a search result from a
+        #specific index. If the index is invalid raise
+        #exception.
+        if index not in data:
+            raise Exception("Index not in range")
+        
+        return data[index]
+
+    def option_map(self, option):
+        #Maps the input option to the column the data is 
+        #found in the database.
+
+        #Column representatons
+        column_order = {
+            "greenspaces":3,
+            "safety":4,
+            "crowdedness":5,
+            "happiness":6,
+            "traffic flow":7
+        }
+
+        if option not in column_order:
+            raise Exception("Invalid filter option")
+
+        return column_order[option]
+
+    def filterSearch(self, data, option):
+        column_index = search.option_map(self, option)
+        old = data[1][column_index]
+        row_count = 1
+
+        for i in range(2, len(data)):
+            if data[i][column_index] > old:
+                row_count += 1
+                old = data[i][column_index]
+
+        return row_count
--- test_models.py
+++ test_models.py
@@ -1,5 +1,5 @@
 import pytest
-
+#Paul Ho Tests
 from models import User
 
 
@@ -32,3 +32,44 @@ class TestUser:
 
     def test_user_id(self):
         assert False
+
+
+#Mazin Abdulmahmood Tests
+from models import search
+
+
+@pytest.fixture
+def input_data():
+    data = {
+            1:["Camden", "LocalPhotoURL", "MapCoordinates", "43", "78"],
+            2:["Hackney", "LocalPhotoURL", "MapCoordinates", "12", "66"],
+            3:["Islington", "LocalPhotoURL", "MapCoordinates", "34", "82"]
+        }
+    return data
+
+class Testsearchclass:
+    def test_getResults(self, input_data):
+        #Test the indexing functionality of getResults
+        result = search.getResult(self, input_data, 2)
+
+        #Index 2 would represent the data for Hackney from the dictionary
+        assert result == ["Hackney", "LocalPhotoURL", "MapCoordinates", "12", "66"]
+
+    def test_getResults2(self):
+        #Test out of range Exception that is raised by class if the index
+        #is out of range
+
+        with pytest.raises(Exception):
+            result = search.getResult(self, input_data, 4)
+    
+    def test_filterSearch(self, input_data):
+        #Test highest entry of chosen option to be filtered
+        result = search.filterSearch(self, input_data, "greenspaces")
+
+        assert result == 1
+
+    def test_filterSearch2(self, input_data):
+        #Test out of range error exception
+        with pytest.raises(Exception):
+            result = search.filterSearch(self, input_data, "pollution")
+        
