ae66a54b628176786558080ee6728841799be601
mazab <mazabdul7@outlook.com>
2021-01-11 20:11:10 +0000

    Mazin Classes Update

--- models.py
+++ models.py
@@ -84,17 +84,17 @@ class User(object):
 class search(object):
 
     def getResult(self, data, index):
-        #Returns the data from a search result from a
-        #specific index. If the index is invalid raise
-        #exception.
+        '''Returns the data from a search result from a
+        specific index. If the index is invalid raise
+        exception.'''
         if index not in data:
             raise Exception("Index not in range")
         
         return data[index]
 
     def option_map(self, option):
-        #Maps the input option to the column the data is 
-        #found in the database.
+        '''Maps the input option to the column the data is 
+        found in the database.'''
 
         #Column representatons
         column_order = {
@@ -111,10 +111,13 @@ class search(object):
         return column_order[option]
 
     def filterSearch(self, data, option):
+        '''Filter search result given option
+        return index of highest option'''
         column_index = search.option_map(self, option)
         old = data[1][column_index]
         row_count = 1
 
+        
         for i in range(2, len(data)):
             if data[i][column_index] > old:
                 row_count += 1
