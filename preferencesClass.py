4487114a60a620cec14121643e25b2e49855d8db
tobyloby12 <tkaterbau@gmail.com>
2021-01-13 00:31:30 +0100

    finishing class and adding a test

--- preferencesClass.py
+++ preferencesClass.py
@@ -2,32 +2,55 @@ class Preferences(object):
 
     # initialising variables from object creation
 
-    def __init__(self, numberOfBedrooms, maxPrice, minPrice, allergies, connectedness):
-        self.numberOfBedrooms = numberOfBedrooms
-        self.maxPrice = maxPrice
-        self.minPrice = minPrice
-        self.allergies = allergies
-        self.wellConnectedness = connectedness
+    def __init__(self, userID):
+        self.userID = userID
+        self.bedrooms = None
+        self.maxPrice = None
+        self.minPrice = None
+        self.allergies = None
+        self.residential = None
+        self.transportPriority = None
 
     # creating set functions to set each value in the class and allow other classes to change parameters in these classes
 
-    def setBedrooms(self, numberOfBedrooms):
-        self.numberOfBedrooms = numberOfBedrooms
+    def setBedrooms(self, bedrooms):
+        self.bedrooms = bedrooms
+
+    def getBedrooms(self):
+        return self.bedrooms
 
     def setMaxPrice(self, maxPrice):
         self.maxPrice = maxPrice
 
+    def getMaxPrice(self):
+        return self.maxPrice
+
     def setAllergies(self, allergies):
         self.allergies = allergies
 
-    def setWellconnectedness(self, connectedness):
-        self.wellConnectedness = connectedness
+    def getAllergies(self):
+        return self.allergies
+
+    def setTransportPriority(self, transportPriority):
+        self.transportPriority = transportPriority
+
+    def getTransportPriority(self):
+        return self.transportPriority
 
     def setMinPrice(self, minPrice):
         self.minPrice = minPrice
 
-    # returning a preference object to be stored within the user class
-    def createPreferences(self):
-        return self
+    def setResidential(self, residential):
+        self.residential = residential
 
-    # this is a comment
+    def getResidential(self):
+        return self.residential
+
+    # returning a preference object to be stored within the user class
+    def loadPreferences(self, databaseValues):
+        # this will load a preference and set all the internal values to those values which hasn't been implemented
+        self.userID, self.bedrooms, self.maxPrice, self.minPrice, self.residential, self.transportPriority, self.allergies = databaseValues
+
+    # saves the values to the database, hasn't been implemented
+    def savePreferences(self):
+        pass
