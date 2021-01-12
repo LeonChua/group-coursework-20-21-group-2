ff3b9571677b8fac8f7f89bf210b58602330a830
mazab <mazabdul7@outlook.com>
2021-01-12 15:02:53 +0000

    updated search class

--- models.py
+++ models.py
@@ -82,6 +82,7 @@ class User(object):
 
 #Mazin Abdulmahmood - Search Class
 class search(object):
+    #Main algorithm yet to be developed
 
     def getResult(self, data, index):
         '''Returns the data from a search result from a
