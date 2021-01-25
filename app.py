c99e0fe4b6f633b70f4b58ab40ac848b1322cd53
mazab <mazabdul7@outlook.com>
2021-01-25 15:14:49 +0000

    flask portfolio update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import flask
 from flask import Flask, render_template, request, url_for, redirect
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import plotly_express as px
+import os
 
 app = Flask(__name__) # creates Flask object from main fcn
 
@@ -25,22 +25,23 @@ def browse():
     df2 = df2.reset_index(drop = True)
     df2["2018/19.2"]= np.array(df2["2018/19.2"], dtype=np.float64)
 
-    # ukavg = df.iloc[[50], [27]] #uk avg hapiness
-    # ukavg = ukavg["2018/19.2"][50]
-    # df2["%Difference from AVG"] = df2["2018/19.2"]
+    ukavg = df.iloc[[50], [27]] #uk avg hapiness
+    ukavg = ukavg["2018/19.2"][50]
+    df2["%Difference from AVG"] = df2["2018/19.2"]
 
-    # for i in range(len(df2["%Difference from AVG"])):
-    #     df2["%Difference from AVG"][i] = (100*(df2["%Difference from AVG"][i] - ukavg)/ukavg)
+    for i in range(len(df2["%Difference from AVG"])):
+        df2["%Difference from AVG"][i] = (100*(df2["%Difference from AVG"][i] - ukavg)/ukavg)
 
-    df2.columns = ["Area", "Happiness", "Latitude", "Longitude"]
+    df2.columns = ["Area", "Happiness", "Latitude", "Longitude", "%Difference from AVG"]
     fig = px.scatter_mapbox(
                             df2, 
                             lat=df2.Latitude, 
                             lon=df2.Longitude, 
                             size="Happiness",  
                             hover_name = "Area",
-                            hover_data= ["Happiness"],
+                            hover_data= ["Happiness", "%Difference from AVG"],
                             color_discrete_sequence=["fuchsia"],
+                            color = "%Difference from AVG",
                             size_max=10, 
                             zoom=9,
                             title= "Happiness Rating per Borough"
@@ -57,6 +58,10 @@ def browse():
 @app.route('/login/')
 def login():
     return render_template("login.html")
+
+@app.route('/graphs/')
+def graphs():
+    return render_template("graphs.html")
 
 if __name__ == '__main__': #runs Flask app
     app.run()
